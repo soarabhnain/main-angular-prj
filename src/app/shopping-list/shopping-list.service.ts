93a6533d0959e50a490b2347613bc2293e1aed84
Soarabh Nain <soarabhnain@gmail.com>
2019-01-28 00:03:16 +0530

    forms initial

--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -2,7 +2,12 @@ import { Ingredient } from '../shared/ingredient.model';
 import { Subject } from 'rxjs/Subject';
 
 export class ShoppingListService {
+
   ingredientsChanged = new Subject<Ingredient[]>();
+
+  startedEditing = new Subject<number>();
+
+
   private ingredients: Ingredient[] = [
     new Ingredient('Apples', 5),
     new Ingredient('Tomatoes', 10),
@@ -10,6 +15,10 @@ export class ShoppingListService {
 
   getIngredients() {
     return this.ingredients.slice();
+  }
+
+  getIngredient(index: number){
+    return this.ingredients[index];
   }
 
   addIngredient(ingredient: Ingredient) {
